7238c582f99935ce53ad7567dc796c5c0532273b
jeggli20 <76450500+jeggli20@users.noreply.github.com>
2023-03-01 15:11:29 -0700

    Templates and Views
    Began working on putting together templates and views

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="author" content="Jacob Eggli" />
+    <meta name="description" content="A data API for the Wingspan board game. All information is received from the Wingspan board game" />
+    <title>Wingspan API</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Amatic+SC&family=Barlow+Condensed&display=swap" rel="stylesheet">
+    <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+</head>
+@include("partials.header")
+<body>
+    <main class="container">
+        @yield("content")
+    </main>
+</body>
+</html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,10 @@
+<header>
+    <h1>Wingspan&nbsp;&nbsp;API</h1>
+    <nav class="navbar">
+        <ul class="nav">
+            <li class="nav-item"><a class="nav-link" href="">Home</a></li>
+            <li class="nav-item"><a class="nav-link" href="">About</a></li>
+            <li class="nav-item"><a class="nav-link" href="">Log In</a></li>
+        </ul>
+    </nav>
+</header>
